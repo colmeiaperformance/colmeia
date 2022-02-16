e86c7383d39d1295ee1e9e88ab576f396dfa015c
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-02-16 11:26:47 -0300

    Mudança do repo - raiz para tema

--- wp-content/themes/colmeia/page.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<?php get_template_part('template-parts/menu-home'); ?>
-<main>
-    <?php get_template_part( 'template-parts/flexible-content' ) ?>
-</main>
-<?php get_footer() ?>
