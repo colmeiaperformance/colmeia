f8a592ff476029c662e18a35c1c71a5015323dbc
brunarafaela <brunarafaelav@outlook.com>
2022-02-18 16:22:38 -0300

    Update sec-product

--- template-parts/sections/products.php
+++ template-parts/sections/products.php
@@ -1,59 +1,37 @@
-<section class="products">
+<section class="products position-relative">
   <div class="container">
   <h2>Produtos</h2>
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+  <div class="row row-cols-1 row-cols-md-3 gy-3 gy-lg-0 gx-5 text-center">
       <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm">
-          <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Free</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
-          </div>
+        <div class="card rounded-0 mb-4 mb-lg-0 border-0 bg-transparent text-white">
+            <div class="icon-square">
+                <img class="" src="<?php bloginfo('stylesheet_directory'); ?>/images/produtos/icon-diagnostico.svg" alt="Diagnóstico">
+            </div>
+            <h4>Diagnóstico</h4>
+            <p>Uma imersão completa na presença digital da sua empresa ou cliente comparada a atuação do mercado e concorrentes. Receba as respostas de como crescer através de uma estratégia integrada.</p>
         </div>
       </div>
+
       <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm">
-          <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Pro</h4>
+        <div class="card rounded-0 mb-4 mb-lg-0 border-0 bg-transparent text-white">
+            <div class="icon-square">
+                <img class="" src="<?php bloginfo('stylesheet_directory'); ?>/images/produtos/icon-buyer-persona.svg" alt="Buyer Persona">
+            </div>
+            <h4>Buyer Persona</h4>
+            <p>Criamos a representação fictícia do seu cliente ideal através de pesquisas e análises concretas. A base de comunicação assertiva em toda a jornada de compra, estratégia de SEO e mídia.</p>
           </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>20 users included</li>
-              <li>10 GB of storage</li>
-              <li>Priority email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
-          </div>
-        </div>
       </div>
+
       <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm border-primary">
-          <div class="card-header py-3 text-white bg-primary border-primary">
-            <h4 class="my-0 fw-normal">Enterprise</h4>
+        <div class="card rounded-0 mb-4 mb-lg-0 border-0 bg-transparent text-white">
+            <div class="icon-square">
+                <img class="" src="<?php bloginfo('stylesheet_directory'); ?>/images/produtos/icon-webinar.svg" alt="Webinar">
+            </div>
+            <h4>Webinar</h4>
+            <p>Público engajado, organização completa e conteúdo potencializado! A metodologia proprietária para fazer os seus seminários online terem resultados incríveis e captação de leads qualificados.</p>
           </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>30 users included</li>
-              <li>15 GB of storage</li>
-              <li>Phone and email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-          </div>
-        </div>
       </div>
+
     </div>
   <div>
-     <hr>
 </section>
