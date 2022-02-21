6f93858cc6fc02ec452c4e6ed9e3081d2459285c
brunarafaela <brunarafaelav@outlook.com>
2022-02-21 17:31:30 -0300

    Add sec-blog

--- footer.php
+++ footer.php
@@ -1,66 +1,61 @@
-<footer class="footer">
-  <div class="container py-5 text-white">
-    <div class="row">
-      <div class="col-lg-2">
-        <h5>Section</h5>
-         <?php
-            wp_nav_menu(array(
-                'theme_location' => 'footer-menu',
-                'container' => false,
-                'menu_class' => '',
-                'fallback_cb' => '__return_false',
-                'items_wrap' => '<ul id="%1$s" class="nav flex-column %2$s">%3$s</ul>',
-                'depth' => 2,
-                'walker' => new bootstrap_5_wp_nav_menu_walker()
-            ));
-            ?>
-      </div>
+<hr class="m-0">
+<footer>
+  <div class="container text-center text-lg-start text-black"> 
+    <div class="py-4 py-lg-5">
+      <div class="row">
+        <div class="col-lg-4 d-none d-lg-block order-0 menu">
+          <?php
+              wp_nav_menu(array(
+                  'theme_location' => 'footer-menu',
+                  'container' => false,
+                  'menu_class' => '',
+                  'fallback_cb' => '__return_false',
+                  'items_wrap' => '<ul id="%1$s" class="nav flex-column %2$s">%3$s</ul>',
+                  'depth' => 2,
+                  'walker' => new bootstrap_5_wp_nav_menu_walker()
+              ));
+              ?>
+        </div>
 
-      <div class="col-lg-2">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-2">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-4 offset-lg-1">
-        <form>
-          <h5>Assine a nossa newsletter</h5>
-          <p>Monthly digest of whats new and exciting from us.</p>
-          <div class="d-flex w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Email</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email">
-            <button class="btn btn-primary" type="button">Cadastrar</button>
+        <div class="col-lg-4 order-2 order-lg-1 social">
+          <p class="mb-0">Want to know more?</p>
+          <p class=""><strong>Follow us on social media!</strong></p>
+          <ul class="list-unstyled d-flex">
+            <li><a href="#">      
+                <img src="<?php echo get_template_directory_uri() . '/images/icon-facebook.svg' ?>" alt="Logo Facebook">
+            </a></li>
+             <li class="ms-3"><a href="#">      
+                <img src="<?php echo get_template_directory_uri() . '/images/icon-instagram.png' ?>" alt="Logo Instagram">
+            </a></li>
+          </ul>
+          <div class="colmeia d-flex align-items-center">
+            <p>Powered by</p>  <a href=""><img src="<?php echo get_template_directory_uri() . '/images/logo-colmeia.svg' ?>" alt="Logo Colmeia"></a>
           </div>
-        </form>
+        </div>
+
+        <div class="col-lg-4 order-1 order-lg-2 infos">
+          <img class="d-block mx-auto mb-3 mb-lg-4" src="<?php echo get_template_directory_uri() . '/images/logo-footer.svg' ?>" alt="Logo" >
+          <ul class="list-unstyled">
+            <li><p><i class="bi bi-geo-alt-fill"></i>  1290 Weston Road, Suit 310, Weston, FL 33326</p></li>
+            <li><p><i class="bi bi-telephone-fill"></i> 333-333-3333</p></li>
+            <li><p><i class="bi bi-envelope-fill"></i> contact@revitta.com</p></li>
+          </ul>
+        </div>
       </div>
     </div>
-
-    <div class="d-flex justify-content-between py-4 my-4 border-top border-secondary">
-      <p>© 2021 Colmeia. Todos os direitos reservados.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-dark" href="#"><i class="bi bi-twitter"></i></a></li> 
-        <li class="ms-3"><a class="link-dark" href="#"><i class="bi bi-facebook"></i></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><i class="bi bi-instagram"></i></a></li>
-      </ul>
-    </div>
-   <p class="float-end"><a href="#">Ir ao topo</a></a></p>
   </div>
+  <div class="copyright"> 
+    <div class="text-center text-uppercase">
+        <p>© 2022 revitta medical & aesthetic. All Rights Reserved.</p>
+        <p class="float-end top">
+          <a href="#">
+            <i class="bi bi-arrow-up-circle-fill"></i>
+          </a>
+         </p> 
+    </div>
+    </div>
+
+
 </footer>
 <?php wp_footer(); ?>
 <script src="js/main.js"></script>
---
+++ template-parts/sections/blog.php
@@ -0,0 +1,5 @@
+<section class="blog">
+  <div class="container">
+    <h2>Blog</h2>
+  </div>
+</section>
