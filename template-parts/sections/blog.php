bf7c952ad5bd1eece2ea922cb2f64a47a259b92a
Igor Fonseca <igor.stutzf@gmail.com>
2022-03-09 16:51:36 -0300

    update sec-blog

--- template-parts/sections/blog.php
+++ template-parts/sections/blog.php
@@ -1,5 +1,48 @@
 <section class="page-blog">
-  <div class="container">
+<div class="container">
     <h2>Blog</h2>
+    <div class=" post row row-cols-2 g-3 flex-column flex-lg-row">
+
+      <div class="col-12 col-lg-6">
+        <div class="d-flex border">
+          <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
+          <div class="content p-3">
+            <p><b>Ferramentas de Automação Titulo maior aqui</b></p>
+            <p>As ferramentas de automação chegaram para ele atender a nova realidade do marketing personalizado...</p>
+            <div class="d-flex justify-content-end">
+              <a href="#" class="btn">
+                Saiba mais
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-lg-6 d-none d-lg-block">
+        <div class="d-flex border">
+          <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
+          <div class="content p-3">
+            <p><b>Ferramentas de Automação Titulo maior aqui</b></p>
+            <p>As ferramentas de automação chegaram para ele atender a nova realidade do marketing personalizado...</p>
+            <div class="d-flex justify-content-end">
+              <a href="#" class="btn">
+                Saiba mais
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
+    <div class="news row mt-5 border py-4">
+        <form class="d-flex justify-content-center align-items-center flex-column flex-md-row">
+          <div class="text-center col-12 col-md-4"><h3>Assine a nossa newsletter</h3></div>
+          <div class="col-12 col-md-6 px-3">
+            <input type="email" placeholder="E-mail" class="form-control" >
+          </div>
+          <button type="submit" class="btn mt-3">Cadastrar</button>
+        </form>
+      </div>
   </div>
 </section>
