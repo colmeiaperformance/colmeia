8ae611b3e6377d96d60fb62952ddb871e19edf0a
brunarafaela <brunarafaelav@outlook.com>
2022-03-10 12:31:03 -0300

    Update sec-blog

--- template-parts/sections/blog.php
+++ template-parts/sections/blog.php
@@ -1,9 +1,9 @@
 <section class="sec-blog">
 <div class="container">
     <h2>Blog</h2>
-    <div class="row row-cols-2 g-3 flex-column flex-lg-row text-white">
+    <div class="row row-cols-2 g-3 flex-column flex-lg-row text-white background-blog">
 
-      <div class="col-12 col-lg-6">
+      <div class="col-12 col-lg-6 background-blog">
         <div class="d-flex border">
           <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
           <div class="content p-3">
@@ -18,7 +18,7 @@
         </div>
       </div>
 
-      <div class="col-12 col-lg-6 d-none d-lg-block">
+      <div class="col-12 col-lg-6 d-none d-lg-block background-blog">
         <div class="d-flex border">
           <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
           <div class="content p-3">
@@ -35,7 +35,7 @@
 
     </div>
     
-    <div class="row mt-5 border py-4 m-1">
+    <div class="row mt-5 border py-4 m-1 background-blog">
         <form class="d-flex justify-content-center align-items-center flex-column flex-lg-row">
           <div class="text-center col-12 col-lg-4 text-white"><h3>Assine a nossa newsletter</h3></div>
           <div class="col-12 col-lg-6 px-3">
