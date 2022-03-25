1239c49ec7e46ec475e8300e8b4c0a8d808295db
brunarafaela <brunarafaelav@outlook.com>
2022-03-25 04:59:53 -0300

    Add page error 404

--- 404.php
+++ 404.php
@@ -1,18 +1,17 @@
 <?php get_header(); ?>
-<?php get_template_part('template-parts/menu-home'); ?>
-<main>
-    <section class="container error text-center pt-5 mt-5">
-			<h1>
-				404
-			</h1>
-			<h2>
-				Página não encontrada :(
-			</h2>
+<?php get_template_part('template-parts/navbar'); ?>
+<main class="error cover-container">
+ <div class="container">
+	  <div class="py-3 pt-lg-5 pb-lg-5 text-white d-flex mx-auto flex-column align-items-center justify-content-center">
+    <h1 class="p-0 pt-lg-5 mb-0">
+			<img src=<?php echo get_template_directory_uri() . '/images/404/404.png' ?> alt="Error 404" width="100%" height="auto">      
+		</h1>
+    <div class="lead">
 			<p>
-				Desculpe, a página que você está procurando não existe.
+				A página que você tentou acessar não pode ser encontrada.
 			</p>
-			<a href="<?php echo home_url(); ?>" class="btn btn-primary btn-lg">
-				Voltar ao início
-			</a>
-	</section>
+		</div>
+      <a href="<?php echo home_url(); ?>" class="btn border-white rounded-0 mb-0 mb-lg-5"><i class="bi bi-arrow-left-short"></i> Voltar</a>
+	</div>
+ </div>
 </main>
