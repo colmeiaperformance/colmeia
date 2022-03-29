b6f84109ea2a59436fffe55166646e85edec11e4
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-03-29 16:03:50 -0300

    Update page-agencias, page-empresas

--- page-empresas.php
+++ page-empresas.php
@@ -2,10 +2,7 @@
 <?php get_template_part('template-parts/navbar'); ?>
 <?php get_template_part('template-parts/headers/header-empresas'); ?>
 <main class="agencias">
-<?php get_template_part('template-parts/sections/products'); ?>
-<?php get_template_part('template-parts/sections/services'); ?>
-<?php get_template_part('template-parts/sections/cases'); ?>
-<?php get_template_part('template-parts/sections/blog'); ?>
+<?php get_template_part('template-parts/flexible-content'); ?>
 
 </main>
 <?php get_footer();
