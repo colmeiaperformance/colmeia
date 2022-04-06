9213bd4f3ea88107e0bbe53eabe8941382f4dbba
brunarafaela <brunarafaelav@outlook.com>
2022-04-05 23:35:11 -0300

    Add services bg logo

--- page-servicos.php
+++ page-servicos.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
   <?php get_template_part('template-parts/navbar'); ?>
   <?php get_template_part('template-parts/headers/header-services'); ?>
-<main class="services">
+<main class="page-services">
   <?php get_template_part('template-parts/sections/services'); ?>
   <?php get_template_part('template-parts/sections/newsletter'); ?>
 </main>
