07ebaa88b4d9cd45edb1bf7a87ef7afc0066bfa2
brunarafaela <brunarafaelav@outlook.com>
2022-04-07 02:05:10 -0300

    Finished services css

--- template-parts/sections/services.php
+++ template-parts/sections/services.php
@@ -1,55 +1,39 @@
 <section class="services">
   <div class="container py-5"><h2 class="m-0">Serviços</h2></div>
   <div class="container pt-5">
-    <div class="row">
+    <div class="row align-items-center justify-content-center">
   
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-         <div class="card rounded-0">
-             <a href="#" class="stretched-link text-decoration-none">Marketing Inbound</a>
-         </div>
+      <div class="card rounded-0">
+        <a href="#" class="stretched-link text-decoration-none">Marketing Inbound</a>
       </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-       <div class="card rounded-0">
+     <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Mídia Online</a>
         </div>
-      </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-        <div class="card rounded-0">
+    <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Search Engine Optimization</a>
         </div>
-      </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-        <div class="card rounded-0">
+     <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Marketing de Conteúdo</a>
         </div>
-      </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-        <div class="card rounded-0">
+       <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Gestão de Redes Sociais</a>
         </div>
-      </div>
 
-        <div class="col col-6 col-lg-3 d-flex align-items-center">
-          <div class="card rounded-0">
+       <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Data Analytics</a> 
         </div>
-      </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-         <div class="card rounded-0">
+      <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Gestão de Leads</a>
         </div>
-      </div>
 
-     <div class="col col-6 col-lg-3 d-flex align-items-center">
-          <div class="card rounded-0">
+    <div class="card rounded-0">
             <a href="#" class="stretched-link text-decoration-none">Automação de Marketing</a>
         </div>
-      </div>
  
     </div>
   </div>
