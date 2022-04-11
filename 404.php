4655671b25fe1388e62642fc14b7300f58e919cd
Igor Fonseca <igor.stutzf@gmail.com>
2022-04-11 17:55:36 -0300

    update page 404

--- 404.php
+++ 404.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/navbar'); ?>
-<main class="error cover-container">
- <div class="container">
-	  <div class="py-3 pt-lg-5 pb-lg-5 text-white d-flex mx-auto flex-column align-items-center justify-content-center">
-    <h1 class="p-0 pt-lg-5 mb-0 text-center">
-			<img src=<?php echo get_template_directory_uri() . '/images/404/404.png' ?> alt="Error 404" width="100%" height="auto">      
-		</h1>
-    <div class="lead">
-			<p>
-				A página que você tentou acessar não pode ser encontrada.
-			</p>
+<main class="error cover-container d-flex mx-auto flex-column align-items-center justify-content-center">
+	
+	<div class="container">
+		<div class="text-white d-flex mx-auto flex-column align-items-center justify-content-center">
+			<div class="d-flex flex-column flex-md-row px-4 align-items-center justify-content-center">
+				<h1 class="text-center align-items-center justify-content-center w-sm-100">
+					Ops!      
+				</h1>
+				<div class="error-text d-flex flex-column justify-content-center px-4 w-sm-100">
+					<h2 class="">404</h2>
+					<p class="" >A página que você tentou acessar não pode ser encontrada.</p>
+				</div>
+			</div>
+			<a href="<?php echo home_url(); ?>" class="btn border-white rounded-0 mb-0 mb-lg-5"><i class="bi bi-arrow-left-short"></i> Voltar</a>
 		</div>
-      <a href="<?php echo home_url(); ?>" class="btn border-white rounded-0 mb-0 mb-lg-5"><i class="bi bi-arrow-left-short"></i> Voltar</a>
 	</div>
- </div>
 </main>
