5a6c38b9d0a625b175762cc3b4b4f4e055c03f42
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-05-10 00:17:21 -0300

    Add php page-contato

--- page-contato.php
+++ page-contato.php
@@ -1,63 +1,26 @@
+<?php
+$imagem_de_fundo_c = get_field('imagem_de_fundo_c');
+$cor_do_fundo_c = get_field('cor_do_fundo_c');
+$subtitulo_c = get_field('subtitulo_c');
+$titulo_c = get_field('titulo_c');
+$texto_c = get_field('texto_c');
+$formulario_c = get_field('formulario_c');
+?>
+
 <?php get_header(); ?>
 <?php get_template_part('template-parts/navbar'); ?>
 <?php get_template_part('template-parts/breadcrumb'); ?>
-<main class="page-contact">
+<main class="page-contact" style="background: <?php if ($cor_do_fundo_c) { echo $cor_do_fundo_c; } ?> url('<?php if ($imagem_de_fundo_c) { echo $imagem_de_fundo_c; } ?>') center center no-repeat; background-size: cover;">
   <div class="container">
     <div class="row d-flex align-items-center justify-content-center">
       <div class="col-12 col-lg-6 pe-lg-5">
         <h1 class="d-none">Contato</h1>
-        <h2>Nossa localização? </br> <strong>Anywhere!</strong></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nascetur ridiculus mus mauris vitae ultricies. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Consectetur libero id faucibus nisl tincidunt eget nullam non.</p>
+        <h2><?php if ( $subtitulo_c ) { echo $subtitulo_c; } ?></br>
+        <strong><?php if ( $titulo_c ) { echo $titulo_c; } ?></strong></h2>
+        <?php if ( $texto_c ) { echo $texto_c; } ?>
       </div>
       <div class="col-12 col-lg-6 ps-lg-5">
-        <form>
-            <div class="row">
-              <div class="col-12 col-md-6 form-group">
-                <label for="exampleInputName">Nome</label>
-                <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Nome">
-              </div>
-              <div class="col-12 col-md-6 form-group">
-                <label for="exampleInputEmail">E-mail</label>
-                <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="E-mail">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12 col-md-6 form-group">
-                <label for="exampleInputPhone">Telefone</label>
-                <input type="tel" class="form-control" id="exampleInputPhone" aria-describedby="phoneHelp" placeholder="Telefone">
-              </div>
-              <div class="col-12 col-md-6 form-group">
-                <select class="form-control" id="exampleFormControlSelectEstado">
-                  <option>Estado</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-            </div>
-          <div class="row">
-            <div class="col-12 col-md-6 form-group">
-              <label for="exampleInputEmpresa">Empresa</label>
-              <input type="text" class="form-control" id="exampleInputEmpresa" aria-describedby="empresaHelp" placeholder="Empresa">
-            </div>
-            <div class="col-12 col-md-6 form-group">
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option>Interesse</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Mensagem</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="1" placeholder="Mensagem"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+        <?php if ( $formulario_c ) { echo $formulario_c; } ?>
       </div>
     </div>
   </div>
