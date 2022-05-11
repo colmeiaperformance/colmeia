6dd6ff78301e28ec2ac1e8cf72e9733ad38a3b4d
brunarafaela <brunarafaelav@outlook.com>
2022-05-11 16:05:44 -0300

    Update services section

--- template-parts/sections/services.php
+++ template-parts/sections/services.php
@@ -5,7 +5,62 @@ $page_address = get_permalink();
 global $post; 
   $slug_name = $post->post_name;
 ?>
+<style>
+  .card {
+    border-left: solid 4px #71C3BD;
+    border-right: solid 4px #71C3BD;
+}
+  .card::before {
+      border-top: solid 5.6569px #71C3BD;
+      border-right: solid 5.6569px #71C3BD;
+  }
+  .card::after {
+      border-bottom: solid 5.6569px #71C3BD;
+      border-left: solid 5.6569px #71C3BD;
+  }
+  .card a {
+    color: #71C3BD;
+  }
+  
+  .card:hover {
+     background-color: #71C3BD;
+  }
 
+  .card:hover  a {
+    color: #fff;
+  }
+
+  .card.active {
+    background: red;
+    border-left: solid 4px red;
+    border-right: solid 4px red;
+  }
+  .card.active a {
+    color: #fff;
+  }
+  .card.active::before {
+    border-top: solid 5.6569px red;
+    border-right: solid 5.6569px red;
+  }
+  .card.active::after {
+    border-bottom: solid 5.6569px red;
+    border-left: solid 5.6569px red;
+  }
+  .card.active:hover {
+    background: red;
+    border-left: solid 4px red;
+    border-right: solid 4px red;
+  }
+  .card.active:hover .card.active::before {
+    border-top: solid 5.6569px red;
+    border-right: solid 5.6569px red;
+  }
+  .card.active:hover .card.active::after {
+    border-bottom: solid 5.6569px red;
+    border-left: solid 5.6569px red;
+  }
+
+</style>
 
 <section class="services">
   <div class="container py-5"><h2 class="m-0"><?php if ($titulo_serv) { echo $titulo_serv; } ?></h2></div>
