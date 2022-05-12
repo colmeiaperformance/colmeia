80bff21ae65457acaa6ef614d42bb7ec437d9127
brunarafaela <brunarafaelav@outlook.com>
2022-05-12 16:29:46 -0300

    Add page-sobre

--- page-sobre.php
+++ page-sobre.php
@@ -1,59 +1,59 @@
-<?php
-$imagem_mobile_qs = get_field('imagem_mobile_qs');
-$card_imagem_qs = get_field('card_imagem_qs');
-$citacao_qs = get_field('citacao_qs');
-?>
-
 <?php get_header(); ?>
   <?php get_template_part('template-parts/navbar'); ?>
   <?php get_template_part('template-parts/breadcrumb'); ?>
 <main class="page-sobre">
-
-
+ <h1 class="d-none">Sobre nós</h1>
   <div class="container">
-    <div class="row d-flex align-items-stretch">
-      <div class="quem-somos col-lg-6 d-flex flex-column text-center text-lg-start my-5 justify-content-left align-items-left px-4">
-        <div class="titulo w-lg-75">
-          <h1>Quem Somos?</h1>
-        </div>
-        <div class="text w-lg-75">
-          <p>Com 7 anos de experiência, a Colmeia atende empresas nacionais e multinacionais de pequeno e grande porte. Também atuamos como parceiros de agências de publicidade e comunicação oferecendo performance e criatividade com foco em alavancar resultados. 
-            <br><br>
-            A gestão de marketing por áreas que não interagem e métricas que não conversam entre si leva ao desperdício de forças e recursos financeiros e, por consequência, de competitividade. Juntar os parceiros certos e trazer uma visão integrada, que potencializa a criatividade e aprofunda o conhecimento, é a nova fórmula do sucesso. É aí que nossa expertise se encaixa. 
-            <br><br>
-            Nossa aposta é em colaboração e complementaridade: um ecossistema que junta diferentes expertises para oferecer um portfólio amplo e efetivo, com a profundidade de conhecimento que cada área exige, tendo o cliente como centro das soluções oferecidas. 
-            
-          </p>
+    <div class="row">
+      <div class="col col-12">
+        <div class="row d-flex">
+          <div class="col-12 col-lg-6 image">        
+            <img src=<?php echo get_template_directory_uri() . '/images/sobre/sobre.png' ?> alt="Sobre">            
+           </div>
+           <div class="col-12 col-lg-6 text">
+              <div class="box">
+                <h2>Quem somos?</h2>
+                <p>Com 7 anos de experiência, <Strong>a Colmeia</Strong> atende empresas <Strong>nacionais</Strong> e <strong>multinacionais</strong> de pequeno e grande porte. Também atuamos como parceiros de <Strong>agências de publicidade e comunicação</Strong> oferecendo performance e criatividade com foco em alavancar resultados.</p>
+              </div>
+           </div>
         </div>
       </div>
-      <div class="col-lg-6 d-flex justify-content-center align-items-center">
-        <div class="card d-flex justify-content-center align-items-center">
-          <div class="testimonial d-flex justify-content-center align-items-center text-center flex-column">
-            <div class="testimonial-text px-5 py-4 mx-2 my-5">
-            <?php if ($citacao_qs['texto_da_citacao']) { echo $citacao_qs['texto_da_citacao']; } ?>
-            </div>
-            <div class="testimonial-autor w-100">
-              <div class="autor-name">
-                <p><b><?php if ($citacao_qs['autor']) { echo $citacao_qs['autor']; } ?></b></p>
+
+      <div class="col col-12">
+        <div class="row d-flex">
+          <div class="col-12 col-lg-6 image">        
+            <img src=<?php echo get_template_directory_uri() . '/images/sobre/sobre.png' ?> alt="Sobre">            
+           </div>
+           <div class="col-12 col-lg-6 text">
+              <div class="box">
+                <p>Juntar os parceiros <strog>certos</strog> e ter uma <strong>visão integrada</strong> do ambiente digital, que potencializa a criatividade e aprofunda o conhecimento, é a <strong>nova fórmula do sucesso</strong> para uma gestão de marketing <stromg>eficiente</stromg>, sem <Stromg>desperdício</Stromg> de forças e recursos financeiros. É aí que nossa expertise se encaixa.</p>
               </div>
-              <div class="autor-office">
-                <p><?php if ($citacao_qs['cargo']) { echo $citacao_qs['cargo']; } ?></p>
-              </div>
-            </div>
-            <div class="icon">
-              <img src="" alt="">
-            </div>
-          </div>
+           </div>
         </div>
-      </div>      
+      </div>
+
+      <div class="col col-12">
+        <div class="row d-flex">
+          <div class="col-12 col-lg-6 image">        
+            <img src=<?php echo get_template_directory_uri() . '/images/sobre/sobre.png' ?> alt="Sobre">            
+           </div>
+           <div class="col-12 col-lg-6 text">
+              <div class="box">
+                <p>Nossa aposta é em colaboração e complementaridade: um <strong>ecossistema</strong> que junta diferentes expertises para oferecer um portfólio <strong>amplo</strong> e <strong>efetivo</strong>, com a profundidade de conhecimento que cada área exige, tendo o cliente como centro das soluções oferecidas.</p>
+              </div>
+           </div>
+        </div>
+      </div>
+
     </div>
   </div>
-  <div class="image-sobre mt-4" style="background: url(images/sobre/sobre.png) top left no-repeat; background-size: cover;">
-    
+  <div class="quote">
+    <img src=<?php echo get_template_directory_uri() . '/images/sobre/sobre-quote.png' ?> alt="Sobre">
+      <div class="container">
+        <p>“Nossa missão é reunir o que existe de melhor em estratégia e produção de conteúdo, ferramentas de mensuração e tecnologias de automação. Após vinte anos de carreira na área de marketing, em empresas de referência como a Coca-Cola Company, Nielsen, Enel e em agências, como DIA Comunicação, estruturei a Colmeia como um verdadeiro hub de inteligência, com foco em performance digital.”</p>
+        <h4>Fernando Terra - Sócio da Colmeia</h4>
+    </div>
   </div>
-
-
-  <?php get_template_part('template-parts/flexible-content'); ?>
-  <?php get_template_part('template-parts/sections/newsletter'); ?>
+  <?php get_template_part('template-parts/sections/blog'); ?>
 </main>
 <?php get_footer();
