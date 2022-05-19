8ed5f8ca0568ce6e2ed77cde78af972aac61deeb
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-05-19 16:13:21 -0300

    Update blog.php

--- template-parts/sections/blog.php
+++ template-parts/sections/blog.php
@@ -1,66 +1,52 @@
 <section class="sec-blog">
   <div class="container">
     <h2>Blog</h2>
-      <div id="oquedizem" class="swiper swiperVisiteBlog" data-bs-ride="carousel">
-        <div class="swiper-wrapper mb-4 pb-4 mb-lg-5 pb-lg-5">
-          <div class="swiper-slide col-12 col-lg-6 background-blog">
-            <div class="d-flex border">
-              <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
-              <div class="content p-3 text-white">
-                <p><strong>Ferramentas de Automação Titulo maior aqui</strong></p>
-                <p>As ferramentas de automação chegaram para ele atender a nova realidade do marketing personalizado...</p>
-                <div class="d-flex justify-content-end">
-                  <a href="#" class="btn text-white background-mustard-dark rounded-0">
+
+    <div id="oquedizem" class="swiper swiperVisiteBlog" data-bs-ride="carousel">
+      <div class="swiper-wrapper mb-4 pb-4 mb-lg-5 pb-lg-5">
+
+        <?php $oquedizem_query = new WP_Query( 'posts_per_page=4' );
+          while ( $oquedizem_query->have_posts() ) : $oquedizem_query->the_post();
+            $do_not_duplicate = $post->ID; ?>
+
+        <div class="swiper-slide col-12 col-lg-6 background-blog">
+          <div class="d-flex border">
+            <img class="img-fluid" src="<?php if ( has_post_thumbnail() ) { echo the_post_thumbnail_url(); } ?>"
+              alt="Metodologia" />
+            <div class="content p-3 text-white">
+              <p><strong><?php the_title(); ?></strong></p>
+              <p><?php the_excerpt(); ?></p>
+              <div class="d-flex justify-content-end">
+                <a href="<?php the_permalink(); ?>" class="btn text-white background-mustard-dark rounded-0">
                   Saiba mais
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="swiper-slide col-12 col-lg-6 background-blog">
-            <div class="d-flex border">
-              <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
-              <div class="content p-3 text-white">
-                <p><strong>Ferramentas de Automação Titulo maior aqui</strong></p>
-                <p>As ferramentas de automação chegaram para ele atender a nova realidade do marketing personalizado...</p>
-                <div class="d-flex justify-content-end">
-                  <a href="#" class="btn text-white background-mustard-dark rounded-0">
-                  Saiba mais
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="swiper-slide col-12 col-lg-6 background-blog">
-            <div class="d-flex border">
-              <img class="img-fluid" src="<?php bloginfo('stylesheet_directory'); ?>/images/cases/post-blog.png" alt="Metodologia" />
-              <div class="content p-3 text-white">
-                <p><strong>Ferramentas de Automação Titulo maior aqui</strong></p>
-                <p>As ferramentas de automação chegaram para ele atender a nova realidade do marketing personalizado...</p>
-                <div class="d-flex justify-content-end">
-                  <a href="#" class="btn text-white background-mustard-dark rounded-0">
-                  Saiba mais
-                  </a>
-                </div>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="swiper-pagination"></div>
+
+        <?php endwhile; 
+        wp_reset_postdata();
+        ?>
+       
+
       </div>
+      <div class="swiper-pagination"></div>
+    </div>
+
     <div class="row mt-2 mt-lg-5 border py-4 m-1 background-blog">
       <form class="d-flex justify-content-center align-items-center flex-column flex-lg-row">
         <div class="text-center col-12 col-lg-4 text-white">
           <h3>Assine a nossa newsletter</h3>
         </div>
         <div class="col-12 col-lg-6 px-3">
-          <input type="email" placeholder="E-mail" class="form-control rounded-0 color-primary-dark" >
+          <input type="email" placeholder="E-mail" class="form-control rounded-0 color-primary-dark">
         </div>
         <button type="submit" class="btn mt-3 mt-lg-0 rounded-0 text-white background-mustard-dark">Cadastrar</button>
       </form>
     </div>
   </div>
-   <script>
+  <script>
   jQuery(document).ready(function() {
     var swiper = new Swiper(".swiperVisiteBlog", {
       slidesPerView: 1,
