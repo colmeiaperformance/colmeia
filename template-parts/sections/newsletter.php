e77e9c1131d708611a016bafcc316b3b417ac7d2
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-05-26 17:20:49 -0300

    Update newsletter php on pages and blog

--- template-parts/sections/newsletter.php
+++ template-parts/sections/newsletter.php
@@ -1,13 +1,12 @@
+<?php
+$codigo_do_formulario_news = get_field('codigo_do_formulario_news', 'option');
+?>
+<?php if ($codigo_do_formulario_news) { ?>
 <section class="newsletter">
   <div class="container">
     <div class="row border py-4 my-1 background-blog">
-      <form class="d-flex justify-content-center align-items-center flex-column flex-lg-row">
-        <div class="text-center col-12 col-lg-4 text-white"><h3>Assine a nossa newsletter</h3></div>
-        <div class="col-12 col-lg-6 px-3">
-          <input type="email" placeholder="E-mail" class="form-control rounded-0 color-primary-dark" >
-        </div>
-        <button type="submit" class="btn mt-3 mt-lg-0 rounded-0 text-white background-mustard-dark">Cadastrar</button>
-      </form>
+      <?php echo $codigo_do_formulario_news; ?>
      </div>
   </div>
 </section>
+<?php } ?>
--- template-parts/sidebar/widgets/newsletter.php
+++ template-parts/sidebar/widgets/newsletter.php
@@ -1,11 +1,10 @@
-    <div class="newsletter d-flex flex-column align-items-center justify-content-center background-blog text-white">
-      <h4>Newsletter</h4>
-      <h5>Lorem Ipsum Subscribers!</h5>
-      <form class="col-12 d-flex flex-column align-items-center justify-content-center">
-        <input type="email" placeholder="E-mail" class="form-control rounded-0 color-primary-dark">
-        <button type="submit" class="btn rounded-0 text-white">Cadastrar</button>
-        <div class="col-10 text-center">
-          <p>Ao cadastrar, você aceitará nossas Política de Privacidade</p>
-        </div>
-      </form>
-    </div>
+<?php
+$codigo_do_formulario_news = get_field('codigo_do_formulario_news', 'option');
+?>
+<?php if ($codigo_do_formulario_news) { ?>
+<div class="newsletter d-flex flex-column align-items-center justify-content-center background-blog text-white">
+  <h4>Newsletter</h4>
+  <h5>Lorem Ipsum Subscribers!</h5>
+  <?php echo $codigo_do_formulario_news; ?>
+</div>
+<?php } ?>
