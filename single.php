dcb2fa596806d60786274e8ee6d4556038bc3dc9
brunarafaela <brunarafaelav@outlook.com>
2022-05-27 02:29:11 -0300

    Update single

--- single.php
+++ single.php
@@ -51,15 +51,15 @@
             </div>
          </div>
           <div class="post-footer">
-            <div class="tags d-flex align-items-center justify-content-start">
+            <!-- <div class="tags d-flex align-items-center justify-content-start">
               <?php $tags = get_tags(); ?>
                 <?php foreach ( $tags as $tag ) { ?>
                 <a href="<?php echo get_tag_link( $tag->term_id ); ?> " rel="tag">#<?php echo $tag->name; ?></a>
                 <?php } ?>
-            </div>
+            </div> -->
             <div class="social">
               <a title="Compartilhe no Facebook" target="_blank"  href="http://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>"><i class="bi bi-facebook"></i></a>
-              <a title="Compartilhe no Twitter"  target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php the_permalink() ?>"><a target="_blank" title="Compartilhe no Twitter"  href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php the_permalink() ?>"><i class="bi bi-twitter"></i></a></a>
+              <a title="Compartilhe no Twitter"  target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php the_permalink() ?>"><i class="bi bi-twitter"></i></a>
               <a title="Compartilhe no Whatsapp" target="_blank" href="https://api.whatsapp.com/send?text=<?php the_title(); ?> - <?php the_permalink() ?>"><i class="bi bi-whatsapp"></i></a>
               <a title="Compartilhe no Linkedin" target="_blank" href="/"><i class="bi bi-linkedin"></i></a>
               <a title="Compartilhe no Telegram" target="_blank" href="/"><i class="bi bi-telegram"></i></a>
