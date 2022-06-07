59853ed8dabbded496ea784e978892aa5627d0a9
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-07 14:54:44 -0300

    Update most popular widget

--- template-parts/sidebar/widgets/most-popular.php
+++ template-parts/sidebar/widgets/most-popular.php
@@ -1,16 +1,16 @@
-<dl class="footer-item">
-        <?php
-        if ( function_exists('wpp_get_mostpopular') ) { ?>
-            <dt><?php _e('Popular posts', 'icare-domain'); ?></dt>
-            <?php
-            /* Get up to the top 5 commented posts from the last 7 days */
-            wpp_get_mostpopular(array(
-                'limit'     => 5,
-                'post_html' => '<dd><a href="{url}">{title}</a></dd>'
-            ));
-        }
-        ?>
-    </dl>
+<?php
+if ( function_exists('wpp_get_mostpopular') ) { ?>
+    <?php
+    /* Get up to the top 5 posts from the last 7 days */
+    wpp_get_mostpopular(array(
+        'limit'     => 3,
+        'order_by' => 'views',
+        'post_type' => 'post',
+        'stats_date' => 1,
+        'post_html' => '<div class="most-popular"><h4 class="underline">Artigos mais lidos</h4><ul class="list-group"><li class="list-group-item position-relative"><h3><a href="{url}" class="stretched-link">{title}</a></h3>{summary}<span>{date}</span></li></div>'
+    ));
+}
+?>
  <!-- <div class="most-popular">
     <h4 class="underline">Artigos mais lidos</h4>
   <ul class="list-group">
