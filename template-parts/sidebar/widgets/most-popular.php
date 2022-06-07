43efa4526719da5aef71ad8f5fcba132ad36947e
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-07 14:44:27 -0300

    Teste de posts populares 3

--- template-parts/sidebar/widgets/most-popular.php
+++ template-parts/sidebar/widgets/most-popular.php
@@ -1,6 +1,20 @@
-  <div class="most-popular">
+<?php
+if ( function_exists('wpp_get_mostpopular') ) { ?>
+    <dt><?php _e('Popular posts', 'icare-domain'); ?></dt>
+    <?php
+    /* Get up to the top 5 commented posts from the last 7 days */
+    wpp_get_mostpopular(array(
+        'limit'     => 3,
+        'order_by' => 'views',
+        'post_type' => 'post',
+        'stats_date' => 1,
+        'post_html' => '<div class="most-popular"><h4 class="underline">Artigos mais lidos</h4><ul class="list-group"><li class="list-group-item position-relative"><h3><a href="{url}" class="stretched-link">{title}</a></h3>{summary}<span>{date}</span></li></div>'
+    ));
+}
+?>
+ <!-- <div class="most-popular">
     <h4 class="underline">Artigos mais lidos</h4>
-   <!-- <ul class="list-group">
+  <ul class="list-group">
       <li class="list-group-item position-relative"> 
         <h3><a href="#" class="stretched-link">Lorem ipsum dolor sit amet, consectetur</a></h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
@@ -17,33 +31,3 @@
         <span>Por: Admin - 01 de Março de 2022</span>
       </li>
     </ul>  -->
-    <?php
-$popularpostbyview = array(
-    'meta_key'  => 'wp_post_views_count', // set custom meta key
-    'orderby'    => 'meta_value_num',
-    'order'      => 'DESC',
-    'posts_per_page' => 4
-);
- 
-// Invoke the query
-$prime_posts = new WP_Query( $popularpostbyview );
- 
-if ( $prime_posts->have_posts() ) :?>
-    <ul>
-        <?php
-            while ( $prime_posts->have_posts() ) : $prime_posts->the_post();
-            ?>
-                <li>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                      <?php the_title(); ?>
-                    </a>
-                </li>
-            <?php
-            endwhile;
-            wp_reset_postdata();
-        ?>
-    </ul>
-<?php 
-endif;
-?>
-  </div>
