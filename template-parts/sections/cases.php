7245fcbfd162b91b390627e2e22cd91ae9813c26
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-07-06 15:53:07 -0300

    Update cases.php link acf

--- template-parts/sections/cases.php
+++ template-parts/sections/cases.php
@@ -11,7 +11,7 @@ $num_case = 1;
     <?php foreach ($item_cases as $ic) { ?>
     
       <div class="col-12 col-lg-4 px-3">
-        <a href="#">
+        <a href="<?php if ( $ic['link_item_cases'] ) { echo $ic['link_item_cases']; } else { echo '#'; } ?>">
         <div class="card rounded-0 bg-transparent text-white">
             <div class="icon d-inline">
                 <img src="<?php if ($ic['imagem_item_cases']) { echo $ic['imagem_item_cases']; } ?>" alt="Case <?php echo $num_case; ?>">
