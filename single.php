72da084501d9908433f09b991fd1ba0ce721c41e
Bruna Rafaela <brunarafaelav@outlook.com>
2022-08-03 10:57:54 -0300

    Update single

--- single.php
+++ single.php
@@ -43,12 +43,12 @@
           </div>
          <div class="post-content">
             <?php the_content(' '); ?>
-            <div class="post-like">
+            <!-- <div class="post-like">
               <p>Esse artigo foi útil?</p> 
               <div>
                 <a href="/">Sim</a> <a href="/">Não</a>
               </div>
-            </div>
+            </div> -->
          </div>
           <div class="post-footer">
             <!-- <div class="tags d-flex align-items-center justify-content-start">
